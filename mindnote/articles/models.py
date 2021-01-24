096cf7b35c20f0d93076a5b14f2b32fc2c0368e1
maitracle <maitracle@gmail.com>
2021-01-25 00:57:41 +0900

    feat: articles app을 추가한다.

---
+++ mindnote/articles/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from commons.models import BaseModel
+
+
+class Article(BaseModel):
+    user = models.ForeignKey('users.User', related_name='articles', on_delete=models.CASCADE)
+    subject = models.CharField(max_length=512)
+    description = models.TextField(blank=True)
+
+
+class Note(BaseModel):
+    article = models.ForeignKey('articles.Article', related_name='notes', on_delete=models.CASCADE)
+    contents = models.TextField(blank=True)
+
+
+class Connection(BaseModel):
+    article = models.ForeignKey('articles.Article', related_name='connections', on_delete=models.CASCADE)
+    left_note = models.ForeignKey('articles.Note', related_name='connections_as_left_side', on_delete=models.CASCADE)
+    right_note = models.ForeignKey('articles.Note', related_name='connections_as_right_side', on_delete=models.CASCADE)
+    reason = models.TextField()
